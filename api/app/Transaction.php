9b945b1b915290f544512567161019f19ac9cd01
Liam Walder <liamwalder@hotmail.co.uk>
2020-04-04 16:35:32 +0100

    Allow currency_id to be fillable and define relationships.

--- api/app/Transaction.php
+++ api/app/Transaction.php
@@ -18,12 +18,37 @@ class Transaction extends Model
         'to',
         'from',
         'amount',
-        'details'
+        'details',
+        'currency_id'
     ];
 
     /**
      * @var bool
      */
     public $timestamps = false;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function to()
+    {
+        return $this->belongsTo(Account::class, 'to', 'id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function from()
+    {
+        return $this->belongsTo(Account::class, 'from', 'id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function currency()
+    {
+        return $this->belongsTo(Currency::class);
+    }
 
 }
